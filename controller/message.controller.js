e736d7609339a0dd7f82e4706af552523b4f5bd5
Azmi <azmi@Azmis-MacBook-Pro.local>
2023-01-13 13:57:42 +0700

    revert

--- controller/message.controller.js
+++ controller/message.controller.js
@@ -1,22 +1,5 @@
 const { createMessage, getAllMessages } = require('../service/message.service');
 
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-        res.status(200).end()
-        return
-    }
-    return await fn(req, res)
-}
-
 const postMessage = async (req, res) => {
     const { userName, text } = req.body;
     const message = await createMessage({ userName, text });
@@ -29,6 +12,6 @@ const getMessages = async (req, res) => {
 }
 
 module.exports = {
-    postMessage: allowCors(postMessage),
-    getMessages: allowCors(getMessages)
+    postMessage,
+    getMessages
 }
